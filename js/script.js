5df66ff7df2809328ea88b6d888d495ac2209d83
MIMIR-GXP <clement.bovin@gmail.com>
2022-10-01 20:24:18 +0200

    Update 1.0

--- js/script.js
+++ js/script.js
@@ -1,9 +1,44 @@
 $(function () {
+
     $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 10 ) {
+        if ( $(window).scrollTop() > 10 && $('#toggle').hasClass('active') === false ) {
             $('.top-page').addClass('active');
         } else {
             $('.top-page').removeClass('active');
         }
     });
+
+    $('#toggle').click(function(){
+        if( $('#toggle').hasClass('active') )  { 
+        $('#toggle').removeClass('active');
+     } 
+     else { 
+        $('#toggle').addClass('active');
+        $('.top-page').removeClass('active');
+     }
+     });
+
+     // Button Menu - open & close
+
+     $(document).ready(function() {
+        $('.close').addClass('active')
+     });
+
+     $('.open').click(function(){
+        $('.open').addClass('active')
+        if ( $('.open').hasClass('active') )  { 
+            $('.close').removeClass('active');
+        }
+        else {
+            $('.close').addClass('active');
+        }
+     });
+
+     $('.close').click(function(){
+        $('.close').addClass('active')
+        if ( $('.close').hasClass('active') )  { 
+            $('.open').removeClass('active');
+        }
+     });
+
 });
